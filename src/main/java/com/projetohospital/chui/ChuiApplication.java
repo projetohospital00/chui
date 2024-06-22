9f37ef4e996ef5005d5dbff1ba95444008c3a730
Eron Moraes <eron.moraes7@gmail.com>
2024-06-22 13:08:08 -0300

    first commit

---
+++ src/main/java/com/projetohospital/chui/ChuiApplication.java
@@ -0,0 +1,13 @@
+package com.projetohospital.chui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetohospital/chui/ChuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetohospital.chui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
