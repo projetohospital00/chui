0de92efdb3f4d34931fafafcb78ec767369ce5dc
Eron Moraes <eron.moraes7@gmail.com>
2024-07-08 11:47:18 -0300

    initial login create

--- Front-Chui/src/app/app.routes.ts
+++ Front-Chui/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "signup",
+    component: SignUpComponent
+  }
+];
---
+++ Front-Chui/src/app/components/defalt-login-layout/defalt-login-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-defalt-login-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './defalt-login-layout.component.html',
+  styleUrl: './defalt-login-layout.component.scss'
+})
+export class DefaltLoginLayoutComponent {
+
+}
