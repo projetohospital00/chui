52a35eb9260b559590ae7fb91edded655dff5db6
Eron Moraes <eron.moraes7@gmail.com>
2024-07-08 12:04:11 -0300

    login page second commit

--- Front-Chui/src/app/app.routes.ts
+++ Front-Chui/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-import { SignUpComponent } from './pages/signup/signup.component';
+//import { SignUpComponent } from './pages/signup/signup.component';
 
 export const routes: Routes = [
   {
@@ -9,6 +9,6 @@ export const routes: Routes = [
   },
   {
     path: "signup",
-    component: SignUpComponent
+    //component: SignUpComponent
   }
 ];
--- Front-Chui/src/app/components/defalt-login-layout/defalt-login-layout.component.ts
+++ Front-Chui/src/app/components/defalt-login-layout/defalt-login-layout.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-defalt-login-layout',
+  selector: 'app-default-login-layout',
   standalone: true,
   imports: [],
   templateUrl: './defalt-login-layout.component.html',
   styleUrl: './defalt-login-layout.component.scss'
 })
-export class DefaltLoginLayoutComponent {
+export class DefaultLoginLayoutComponent {
+  @Input() title: string = "";
+  @Input() primaryBtnText: string = "";
+  @Input() secondaryBtnText: string = "";
+  @Input() disablePrimaryBtn: boolean = true;
+  @Output("submit") onSubmit = new EventEmitter();
 
+  @Output("navigate") onNavigate = new EventEmitter();
+
+  submit(){
+    this.onSubmit.emit();
+  }
+
+  navigate(){
+    this.onNavigate.emit();
+  }
 }
