3b431447c0960040730152390f6093986bc68807
Eron Moraes <eron.moraes7@gmail.com>
2024-07-16 15:12:05 -0300

    controllers 1

---
+++ chui-api/src/main/java/com/chui/chui_api/controllers/AuthController.java
@@ -0,0 +1,55 @@
+package com.chui.chui_api.controllers;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.chui.chui_api.domain.user.User;
+import com.chui.chui_api.dto.LoginRequestDTO;
+import com.chui.chui_api.infra.security.TokenService;
+import com.chui.chui_api.repositories.UserRepository;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginRequestDTO body){
+        User user = this.repository.findByEmail(body.email()).orElseThrow(() -> new RuntimeException("User not found"));
+        if(passwordEncoder.matches(body.password(), user.getPassword())) {
+            String token = this.tokenService.generateToken(user);
+            return ResponseEntity.ok(new ResponseDTO(user.getName(), token));
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody RegisterRequestDTO body){
+        Optional<User> user = this.repository.findByEmail(body.email());
+
+        if(user.isEmpty()) {
+            User newUser = new User();
+            newUser.setPassword(passwordEncoder.encode(body.password()));
+            newUser.setEmail(body.email());
+            newUser.setName(body.name());
+            this.repository.save(newUser);
+
+            String token = this.tokenService.generateToken(newUser);
+            return ResponseEntity.ok(new ResponseDTO(newUser.getName(), token));
+        }
+        return ResponseEntity.badRequest().build();
+    }
+}
---
+++ chui-api/src/main/java/com/chui/chui_api/dto/LoginRequestDTO.java
@@ -0,0 +1,3 @@
+package com.chui.chui_api.dto;
+
+public record LoginRequestDTO (String email, String password){}
